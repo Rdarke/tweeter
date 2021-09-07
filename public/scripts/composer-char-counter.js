71983bca64da0e4325ab3dab7fc4eec5fcf57d42
Rdarke <rickydarke1@gmail.com>
2021-09-07 16:17:14 -0700

    Add notes & refactor counter.js file

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,23 @@
-
+// When HTML doc has been read. 
+// Main function changes the numerical character limit shown to the user 
+// & updates this font color if number Exceeded maximum character limit.
 
 $(document).ready(function() {
+  
   const $tweetText = $("#tweet-text")
   
   $tweetText.on("keyup", function() {
     const characters = this.textLength;
-    console.log(characters);
-
     const $counter = $(this).closest("form").find(".counter");
-
+    
     const charactersMax = 140;
     $counter.val(charactersMax - characters);
 
     if (characters > charactersMax) {
-      console.log("The counter is below zero!!");
       $counter.css("color", "red");
+    }
+    if (characters < charactersMax) {
+      $counter.css("color", "#545149");
     }
   })
 });
