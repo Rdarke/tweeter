5a52a2cfb88b56e63315e3e77549e1017c278e25
Rdarke <rickydarke1@gmail.com>
2021-09-08 18:30:47 -0700

    Update client.js with AJAX Get request logic (FetchTweets)

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,25 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Hard coded tweet object - short term. 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 
 $(document).ready(function() {
+
+  // AJAX to fetch (GET) data from the server.
+  const fetchTweets = () => {
+
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: (tweets) => {
+        console.log("Tweets", tweets)
+        renderTweets(tweets);
+      },
+      error: (err) => {
+        console.log(`Error: ${err}`)
+      }
+    })
+  };
 
   //Takes in a object and returns a tweet <article> element. This element includes HTML tweet structure.
   const createTweetElement = function(obj) {
@@ -68,8 +59,8 @@ $(document).ready(function() {
       $('#tweets-container').append($tweet);
     }
   }
-  renderTweets(tweetData);
-
+  
+  // AJAX for sending (POST) the tweet text to the server.
   const $form = $('#new-tweet-form');
   $form.on('submit', function(event) {
     event.preventDefault();
@@ -82,5 +73,7 @@ $(document).ready(function() {
       console.log(response)
     })
   });
+
+  fetchTweets();
 
 });
